1c77c13493997cf08af5b4346f37b31ddd048547
divvs <frontendnomad@gmail.com>
2025-04-23 17:04:54 +0600

    add tinybase w/ pglite persisting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { PGlite } from '@electric-sql/pglite'
+import { useState } from 'react'
+import { createStore } from 'tinybase'
+import { createPglitePersister } from 'tinybase/persisters/persister-pglite'
+import { Provider, useCreatePersister, useCreateStore } from 'tinybase/ui-react'
+import { Inspector } from 'tinybase/ui-react-inspector'
+
 function App() {
+  const [initializing, setInitializing] = useState(true)
+
+  const store = useCreateStore(createStore)
+
+  useCreatePersister(store, async (store) => {
+    const pglite = await PGlite.create('idb://aster')
+    const persister = await createPglitePersister(store, pglite)
+
+    await persister.startAutoLoad()
+    await persister.startAutoSave()
+
+    setInitializing(false)
+    return persister
+  })
 
   return (
-    <div>
-      clean
-    </div>
+    <Provider store={store}>
+      {initializing ? <div>Loading...</div> : <div>clean</div>}
+      <Inspector />
+    </Provider>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 
-import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true, // WSL requires
+    },
+  },
+  optimizeDeps: { exclude: ['@electric-sql/pglite'] },
 })
